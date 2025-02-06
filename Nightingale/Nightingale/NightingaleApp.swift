e0c9ea61b42fa96f77f618b0d7d9002acb55606d
Simon Myhre <simonmyhre1@gmail.com>
2025-02-06 09:31:25 +0100

    Revert beginning file

--- Nightingale/Nightingale/NightingaleApp.swift
+++ Nightingale/Nightingale/NightingaleApp.swift
@@ -1,29 +1,7 @@
 import SwiftUI
-import FirebaseCore
-
-// Add AppDelegate for Firebase initialization
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        let options = FirebaseOptions(
-            googleAppID: FirebaseConfig.googleAppID,
-            gcmSenderID: FirebaseConfig.gcmSenderID
-        )
-        options.apiKey = FirebaseConfig.apiKey
-        options.projectID = FirebaseConfig.projectID
-        options.storageBucket = FirebaseConfig.storageBucket
-        
-        FirebaseApp.configure(options: options)
-        return true
-    }
-}
 
 @main
 struct NightingaleApp: App {
-    // Register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
