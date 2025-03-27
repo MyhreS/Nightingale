fd12b073eb8f3a7761cd06d99ee98054f6b4168c
Simon Myhre <simonmyhre1@gmail.com>
2025-03-27 11:01:56 +0100

    Fix playlist selector

--- Nightingale/Nightingale/Views/MusicSelector/PlaylistsSelector.swift
+++ Nightingale/Nightingale/Views/MusicSelector/PlaylistsSelector.swift
@@ -2,19 +2,23 @@ import SwiftUI
 
 struct PlaylistsSelector: View {
     @Binding var selectedPlaylist: String
-    
+    @ObservedObject private var musicLibrary = MusicLibrary.shared
+
+    private var playlists: [String] {
+        let nonEmptyPlaylists = musicLibrary.songs.map { $0.playlist }.filter { !$0.isEmpty }
+        let uniquePlaylists = Set(nonEmptyPlaylists)
+        return ["All"] + uniquePlaylists.sorted()
+    }
+
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 10) {
-                TagButton(tag: "All", isSelected: selectedPlaylist == "All") {
-                    selectedPlaylist = "All"
-                    provideHapticFeedback()
-                }
-                
-                    TagButton(tag: "Some playlist", isSelected: selectedPlaylist == "Some playlist") {
-                        selectedPlaylist = "Some playlist"
+                ForEach(playlists, id: \.self) { playlist in
+                    TagButton(tag: playlist, isSelected: selectedPlaylist == playlist) {
+                        selectedPlaylist = playlist
                         provideHapticFeedback()
                     }
+                }
             }
             .padding(.horizontal, 5)
         }
